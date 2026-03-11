5c6a3cf32ff200a0f2569848c57c33230af3a0ef
emullzer <emeric.muller3@gmail.com>
2026-03-11 12:43:09 +0100

    Changements sur l'affichage des mains, ajout du css, et affichage des cartes

--- poker/src/app/mains/mains.ts
+++ poker/src/app/mains/mains.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MainService } from '../service/mains';
+import {Main} from '../model/main';
 
 @Component({
   selector: 'app-mains',
@@ -9,9 +10,13 @@ import { MainService } from '../service/mains';
 })
 export class Mains {
   different_mains: MainService;
+  mainDetails: Main | null = null;
 
   constructor() {
     this.different_mains = new MainService();
   }
 
+  afficherInfos(main: Main) {
+    this.mainDetails = main;
+  }
 }
